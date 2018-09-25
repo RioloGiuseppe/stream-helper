5a5f805af47334888880f6c6503f015f590ec63f
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2018-09-25 22:16:51 +0200

    added stream builder

--- src/stream-builder.ts
+++ src/stream-builder.ts
@@ -1,15 +1,23 @@
-import { TransformCallback, Writable } from "stream";
+import { TransformCallback, Writable, Transform } from "stream";
+import { Serializable } from "byte-serializer";
 
-export class StreamBuilder extends Writable {
+export class StreamBuilder extends Transform {
+
+    public startByte: number;
+    public crcFunction: (data: Buffer) => Buffer;
 
     constructor() {
-        super({
-            objectMode: true
-        });
+        super({ objectMode: true });
     }
 
-    public _transform(chunk: any, encoding: string, callback: TransformCallback): void {
-        console.log(chunk)
+    public _transform(chunk: Serializable, encoding: string, callback: TransformCallback): void {
+        if (chunk instanceof Serializable) {
+            let p = chunk.serialize();
+            let ch = typeof this.crcFunction !== 'function' ? [] : this.crcFunction(p)
+            let b = Buffer.from([this.startByte, chunk.bufferLength, ...p, ...ch]);
+            this.push(b);
+        }
+
         callback();
     }
 
