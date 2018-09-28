7d33e987d74d70664c6e80485011344c28bb9f8c
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2018-09-29 00:02:37 +0200

    fix build sample

--- examples/build-examlple.ts
+++ examples/build-examlple.ts
@@ -1,18 +1,21 @@
-import { StreamBuilder } from "../src/stream/stream-builder";
-import { DataTest } from "../examples/data-test";
+import { PayloadManager, StreamBuilder } from "../src";
+import { DataTest } from "./data-test";
+import { CRC } from 'crc-full';
 
 import { createWriteStream } from "fs";
-import { CRC } from 'crc-full';
-import { PayloadManager } from "../src";
+
+let writerTest = createWriteStream("test-out.txt");
 
 var plManager = new PayloadManager();
 plManager.registerMessage(2, new DataTest());
+
 let builder = new StreamBuilder(plManager);
 builder.startByte = 0x01;
-builder.checksum = (d:Buffer) => CRC.default("CRC16_CCIT_ZERO").computeBuffer(d);
-let writerTest = createWriteStream("test-out.txt");
+builder.checksum = (d: Buffer) => CRC.default("CRC16_CCIT_ZERO").computeBuffer(d);
+
+builder.pipe(writerTest);
+
 let data = new DataTest();
 data.number = 55;
 data.text = "Hello!"
-builder.pipe(writerTest);
 builder.write(data);
