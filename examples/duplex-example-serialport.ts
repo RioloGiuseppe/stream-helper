00a13d9b96f68c1c69068fceea3830f5a196ead6
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2018-09-30 12:08:53 +0200

    modified duplex parser

--- examples/duplex-example-serialport.ts
+++ examples/duplex-example-serialport.ts
@@ -2,6 +2,14 @@ import { PayloadManager, StreamDuplex } from "../src";
 import { DataTest } from "./data-test";
 import { CRC } from 'crc-full';
 
+import * as SerialPort from 'serialport'
+
+var streamIO = new SerialPort('COM13', {
+    baudRate: 9600
+}, (e) => {
+    console.error(e);
+});
+
 
 var plManager = new PayloadManager();
 plManager.registerMessage(2, new DataTest());
@@ -10,4 +18,14 @@ var duplex = new StreamDuplex(plManager);
 duplex.startByte = 0x01;
 duplex.checksum = (d: Buffer) => CRC.default('CRC16_CCIT_ZERO').computeBuffer(d);
 
+streamIO.pipe(duplex);
+duplex.builder.pipe(streamIO);
 
+duplex.on("warn", (e) => console.warn(e));
+duplex.on("data", (c) => console.info(c));
+
+let data = new DataTest();
+data.number = 85;
+data.text = "hello!";
+
+setInterval(() => duplex.send(data), 1000);
